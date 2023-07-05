7c20e7697fb43f9a4888f5c7ec370e9047286b57
Joksa <acajoksimovic70@gmail.com>
2023-07-06 01:25:13 +0200

    Made a simple sign up page with form, installed mongoose module

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ app.listen(3001)
 app.get('/', (req,res) => {
     res.render('index');
 })
+
+app.get('/signUp', (req, res) => {
+    res.render('signUp');
+})
+
+app.post('/signUp', (req, res) => {
+    
+});
+
 app.set('view engine', 'ejs');
