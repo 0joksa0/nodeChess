732a84068ca4938dc098e79f856d04464d86afd8
Joksa <acajoksimovic70@gmail.com>
2023-07-06 01:54:49 +0200

    made a connection to a db and json model for an user

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const app = express();
 
+//const for mongo db url
+const dbUrl = 'mongodb+srv://chessGame:chess123@nodecc.9ciscng.mongodb.net/?retryWrites=true&w=majority'
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then( (result) => {
+            console.log("connection established to mongodb"); 
+            app.listen(3001);
+            console.log("listening on port 3001"); 
+        })
+        .catch( (err) => {console.log("error connecting to mongodb: " + err);})
+
+
+
+//static middleware
 app.use(express.static('public'));
-app.listen(3001)
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
     res.render('index');
@@ -14,7 +32,7 @@ app.get('/signUp', (req, res) => {
 })
 
 app.post('/signUp', (req, res) => {
-    
+    const user = new User(req.body);
+    user.save();
+    res.send(user);
 });
-
-app.set('view engine', 'ejs');
